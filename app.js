d9bdf97cdef1dcdd6c951333a89d8b09dd96a4d7
josesanchez999 <josejsanchez22@icloud.com>
2022-09-02 12:39:31 -0500

    changes

---
+++ app.js
@@ -0,0 +1,9 @@
+var globalVar = "Payments"; //This is the main subject (global variable)
+
+console.log(globalVar);
+
+const price1 = 35000; //Tesla's Price (block)
+const price2 = 12000; //Money that you want to put in already (block)
+let total = price1 - price2; //The math section (block)
+document.getElementById("demo").innerHTML =
+"The total is: " + total; //String variable
